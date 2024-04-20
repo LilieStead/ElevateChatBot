6b0d8d00936bd969ee48f432f235af6cf1e16340
Lilie Stead <liliestead@hotmail.com>
2024-04-21 00:40:36 +0100

    JQuery UI update

--- model.py
+++ model.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 
 # The neural network model below
 class Network(nn.Module):
-    def __init__(self, inputSize, hiddenSize, numberClasses):
+    def __init__(self, inputSize, hiddenSize,  outputSize):
         super(Network, self).__init__()  # Call the parent class constructor
         # Define all three layers of the network
         self.l1 = nn.Linear(inputSize, hiddenSize)
         self.relu = nn.ReLU()  # Correct ReLU activation definition
         self.l2 = nn.Linear(hiddenSize, hiddenSize)
-        self.l3 = nn.Linear(hiddenSize, numberClasses)
+        self.l3 = nn.Linear(hiddenSize,  outputSize)
 
     # Forward pass of the network (send data through layers)
     def forward(self, x):
